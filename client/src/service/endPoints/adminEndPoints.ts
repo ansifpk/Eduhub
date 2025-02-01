cf02f18d4799e5adbdef972400b07bbbd5c60043
ansifpk <pkansif39@gmail.com>
2025-02-01 09:23:56 +0530

    change endpoint for admin

--- client/src/service/endPoints/adminEndPoints.ts
+++ client/src/service/endPoints/adminEndPoints.ts
@@ -1,35 +1,35 @@
 const adminRoutes = {
-    login:'/auth/auth/admin/login',
-    category:'/category/category/admin/category',
-    addCategory:'/category/category/admin/addCategory',
-    editCategory:'/category/category/admin/editCategory',
-    listCategory:'/category/category/admin/listCategory',
-    students:'/profile/profile/admin/students',
-    blockUser:'/auth/auth/admin/blockUser',
-    instructors:'/profile/profile/admin/instructors',
-    instructorAprovel:'/profile/profile/admin/instructorAprovel',
-    blockInstructor:'/auth/auth/admin/blockinstructor',
+    login:'/auth/admin/login',
+    category:'/category/admin/category',
+    addCategory:'/category/admin/addCategory',
+    editCategory:'/category/admin/editCategory',
+    listCategory:'/category/admin/listCategory',
+    students:'/profile/admin/students',
+    blockUse:'/auth/admin/blockUser',
+    instructors:'/profile/admin/instructors',
+    instructorAprovel:'/profile/admin/instructorAprovel',
+    blockInstructor:'/auth/admin/blockinstructor',
     editProfile:"/admin/editProfile",
-    getCourses:"/course/course/admin/getCourses",
-    course:"/course/course/admin/course",
+    getCourses:"/course/admin/getCourses",
+    course:"/course/admin/course",
     logout:'/admin/logout',
-    coupon:'/course/course/admin/coupon',
-    report:'/course/course/admin/report',
-    top5RatedCourse:'/course/course/admin/top5Course',
+    coupon:'/course/admin/coupon',
+    report:'/course/admin/report',
+    top5RatedCourse:'/course/admin/top5Course',
      //!message
-     message:"/message/message/message",
-     notification:"/message/message/notification",
+     message:"/message/message",
+     notification:"/message/notification",
      //chat
-    chat:"/message/message/chat",
-    privetChat:"/message/message/chat/privetChat",
+    chat:"/message/chat",
+    privetChat:"/message/chat/privetChat",
      //*subscription
-     subscription:"/purchase/purchase/admin/subscription",
+     subscription:"/purchase/admin/subscription",
      //*salesReports
-     salesReports:"/purchase/purchase/admin/salesReports",
+     salesReports:"/purchase/admin/salesReports",
      //*order
-     order:"/purchase/purchase/admin/order",
+     order:"/purchase/admin/order",
      //*top5Instructors
-     top5Instructors:"/profile/profile/admin/top5Instructors",
+     top5Instructors:"/profile/admin/top5Instructors",
 
    }
 
