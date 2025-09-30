aad8795c74bc5db410161b0b61efa6a013c3627e
ansifpk <pkansif39@gmail.com>
2025-09-30 11:06:11 +0530

    start styling component

--- hello/src/pages/user/AssesmentTest.tsx
+++ hello/src/pages/user/AssesmentTest.tsx
@@ -7,6 +7,7 @@ import {
   CarouselPrevious,
   type CarouselApi,
 } from "@/components/ui/carousel";
+const Header = React.lazy(() => import('@/components/user/Header'));
 import { testSchema, type TestFormInputs } from '@/util/schemas/testScheema';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -26,6 +27,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Loader2Icon } from 'lucide-react';
 import userRoutes from '@/service/endPoints/userEndPoints';
+
 
 
 
@@ -109,180 +111,186 @@ const AssesmentTest = () => {
   }, [err]);
 
   return (
-    <div className="p-2 space-y-2">
-          <div className=" flex flex-col items-center w-full">
-            <div className=" text-center text-sm text-black ">
-              <div className="font-extrabold text-3xl underline">Add Test</div>
-              Slide {current} of {count}
-            </div>
-            <Carousel setApi={setApi} className="w-[75%] h-[430px] ">
-              <CarouselContent>
-                {watch("questions").map((_value, index) => (
-                  <CarouselItem key={index}>
-                    <Card className=" text-black">
-                      <CardContent>
-                        <strong>Question {index + 1}</strong>
-                        <form
-                          className="space-y-3"
-                          onSubmit={handleSubmit(handleQuestions)}
-                        >
-                          <div>
-                            <textarea
-                              {...register(`questions.${index}.question`)}
-                              className="border border-black w-full rounded h-40 p-2"
-                              placeholder="Enter question"
-                            />
-                            <span className="text-red-500 text-sm">
-                              {errors.questions?.[index]?.question?.message}
-                            </span>
-                          </div>
-                          <div className="grid grid-cols-2 gap-2">
+    <div>
+        <Header />
+        <div className="p-2 space-y-2">
+            <div className=" flex flex-col items-center w-full">
+                <div className=" text-center text-sm text-black ">
+                  <div className="mb-4">
+                    <span className="text-sm font-medium text-blue-500 bg-blue-100 px-3 py-1 rounded-full">
+                        Question 1
+                    </span>
+                  </div>
+                </div>
+                <Carousel setApi={setApi} className="w-[75%] h-[430px]">
+                <CarouselContent>
+                    {watch("questions").map((_value, index) => (
+                    <CarouselItem key={index}>
+                        <Card className=" text-black">
+                        <CardContent>
+                            <strong>Question {index + 1}</strong>
+                            <form
+                            className="space-y-3"
+                            onSubmit={handleSubmit(handleQuestions)}
+                            >
                             <div>
-                              <input
-                                {...register(`questions.${index}.option1`)}
-                                type="text"
-                                placeholder="option 1"
-                                className="border border-black rounded p-2 w-full"
-                              />
-                              <span className="text-red-500 text-sm">
-                                {errors.questions?.[index]?.option1?.message}
-                              </span>
+                                <textarea
+                                {...register(`questions.${index}.question`)}
+                                className="border border-teal-600 w-full rounded h-40 p-2"
+                                placeholder="Enter question"
+                                />
+                                <span className="text-red-500 text-sm">
+                                {errors.questions?.[index]?.question?.message}
+                                </span>
+                            </div>
+                            <div className="grid grid-cols-2 gap-2">
+                                <div>
+                                <input
+                                    {...register(`questions.${index}.option1`)}
+                                    type="text"
+                                    placeholder="option 1"
+                                    className="border border-teal-600 rounded p-2 w-full"
+                                />
+                                <span className="text-red-500 text-sm">
+                                    {errors.questions?.[index]?.option1?.message}
+                                </span>
+                                </div>
+                                <div>
+                                <input
+                                    {...register(`questions.${index}.option2`)}
+                                    type="text"
+                                    placeholder="option 2"
+                                    className="border border-teal-600 rounded w-full p-2"
+                                />
+                                <span className="text-red-500 text-sm">
+                                    {errors.questions?.[index]?.option2?.message}
+                                </span>
+                                </div>
+                                <div>
+                                <input
+                                    {...register(`questions.${index}.option3`)}
+                                    type="text"
+                                    placeholder="option 3"
+                                    className="border border-teal-600 w-full rounded p-2"
+                                />
+                                <span className="text-red-500 text-sm">
+                                    {errors.questions?.[index]?.option3?.message}
+                                </span>
+                                </div>
+                                <div>
+                                <input
+                                    {...register(`questions.${index}.option4`)}
+                                    type="text"
+                                    placeholder="option 4"
+                                    className="border border-teal-600 w-full rounded p-2"
+                                />
+                                <span className="text-red-500 text-sm">
+                                    {errors.questions?.[index]?.option4?.message}
+                                </span>
+                                </div>
                             </div>
                             <div>
-                              <input
-                                {...register(`questions.${index}.option2`)}
+                                <input
                                 type="text"
-                                placeholder="option 2"
-                                className="border border-black rounded w-full p-2"
-                              />
-                              <span className="text-red-500 text-sm">
-                                {errors.questions?.[index]?.option2?.message}
-                              </span>
+                                {...register(`questions.${index}.answer`)}
+                                className="border border-teal-600 p-2 w-full rounded"
+                                placeholder="Enter answer"
+                                />
+                                <span className="text-red-500 text-sm">
+                                {errors.questions?.[index]?.answer?.message}
+                                </span>
                             </div>
-                            <div>
-                              <input
-                                {...register(`questions.${index}.option3`)}
-                                type="text"
-                                placeholder="option 3"
-                                className="border border-black w-full rounded p-2"
-                              />
-                              <span className="text-red-500 text-sm">
-                                {errors.questions?.[index]?.option3?.message}
-                              </span>
-                            </div>
-                            <div>
-                              <input
-                                {...register(`questions.${index}.option4`)}
-                                type="text"
-                                placeholder="option 4"
-                                className="border border-black w-full rounded p-2"
-                              />
-                              <span className="text-red-500 text-sm">
-                                {errors.questions?.[index]?.option4?.message}
-                              </span>
-                            </div>
-                          </div>
-                          <div>
-                            <input
-                              type="text"
-                              {...register(`questions.${index}.answer`)}
-                              className="border border-black p-2 w-full rounded"
-                              placeholder="Enter answer"
-                            />
-                            <span className="text-red-500 text-sm">
-                              {errors.questions?.[index]?.answer?.message}
-                            </span>
-                          </div>
-                          <div className="flex justify-center-safe gap-4">
-                            <AlertDialog>
-                              <AlertDialogTrigger className="bg-black cursor-pointer text-white rounded p-2">
-                                Cancell
-                              </AlertDialogTrigger>
-                              <AlertDialogContent>
-                                <AlertDialogHeader>
-                                  <AlertDialogTitle>
-                                    Are you absolutely sure?
-                                  </AlertDialogTitle>
-                                  <AlertDialogDescription>
-                                    This will discard all the datas you entered
-                                  </AlertDialogDescription>
-                                </AlertDialogHeader>
-                                <AlertDialogFooter>
-                                  <AlertDialogCancel
-                                    className="bg-black cursor-pointer text-white rounded p-2"
-                                    type="button"
-                                  >
-                                    Cancel
-                                  </AlertDialogCancel>
-                                  <AlertDialogCancel
-                                    className="bg-black cursor-pointer text-white rounded p-2"
-                                    type="submit"
-                                    onClick={() => navigate(-1)}
-                                  >
-                                    Continue
-                                  </AlertDialogCancel>
-                                </AlertDialogFooter>
-                              </AlertDialogContent>
-                            </AlertDialog>
-                            {index == 4 && (
-                              <AlertDialog>
-                                <AlertDialogTrigger asChild>
-                                  <button
-                                    type="button"
-                                    className="bg-black cursor-pointer text-white rounded p-2"
-                                    disabled={loading}
-                                  >
-                                    {loading ? (
-                                      <span className="flex items-center-safe">
-                                        Loading...
-                                        <Loader2Icon className="animate-spin size-4" />{" "}
-                                      </span>
-                                    ) : (
-                                      "Submit"
-                                    )}
-                                  </button>
+                            <div className="flex justify-center-safe gap-4">
+                                <AlertDialog>
+                                <AlertDialogTrigger className="bg-teal-600 cursor-pointer text-white rounded p-2">
+                                    Cancell
                                 </AlertDialogTrigger>
                                 <AlertDialogContent>
-                                  <AlertDialogHeader>
+                                    <AlertDialogHeader>
                                     <AlertDialogTitle>
-                                      Are you absolutely sure?
+                                        Are you absolutely sure?
                                     </AlertDialogTitle>
                                     <AlertDialogDescription>
-                                      This will save the dats you provide in our server of Eduhub
+                                        This will discard all the datas you entered
                                     </AlertDialogDescription>
-                                  </AlertDialogHeader>
-                                  <AlertDialogFooter>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
                                     <AlertDialogCancel
-                                      className="bg-black cursor-pointer text-white"
-                                      type="button"
+                                        className="bg-teal-600 cursor-pointer text-white rounded p-2"
+                                        type="button"
                                     >
-                                      Cancel
+                                        Cancel
                                     </AlertDialogCancel>
                                     <AlertDialogCancel
-                                      className="bg-black cursor-pointer text-white"
-                                      type="submit"
-                                      onClick={() =>
-                                        handleSubmit(handleQuestions)()
-                                      }
+                                        className="bg-teal-600 cursor-pointer text-white rounded p-2"
+                                        type="submit"
+                                        onClick={() => navigate(-1)}
                                     >
-                                      Continue
+                                        Continue
                                     </AlertDialogCancel>
-                                  </AlertDialogFooter>
+                                    </AlertDialogFooter>
                                 </AlertDialogContent>
-                              </AlertDialog>
-                            )}
-                          </div>
-                        </form>
-                      </CardContent>
-                    </Card>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
-            </Carousel>
-          </div>
+                                </AlertDialog>
+                                {index == 4 && (
+                                <AlertDialog>
+                                    <AlertDialogTrigger asChild>
+                                    <button
+                                        type="button"
+                                        className="bg-teal-600 cursor-pointer text-white rounded p-2"
+                                        disabled={loading}
+                                    >
+                                        {loading ? (
+                                        <span className="flex items-center-safe">
+                                            Loading...
+                                            <Loader2Icon className="animate-spin size-4" />{" "}
+                                        </span>
+                                        ) : (
+                                        "Submit"
+                                        )}
+                                    </button>
+                                    </AlertDialogTrigger>
+                                    <AlertDialogContent>
+                                    <AlertDialogHeader>
+                                        <AlertDialogTitle>
+                                        Are you absolutely sure?
+                                        </AlertDialogTitle>
+                                        <AlertDialogDescription>
+                                        This will save the dats you provide in our server of Eduhub
+                                        </AlertDialogDescription>
+                                    </AlertDialogHeader>
+                                    <AlertDialogFooter>
+                                        <AlertDialogCancel
+                                        className="bg-teal-600 cursor-pointer text-white"
+                                        type="button"
+                                        >
+                                        Cancel
+                                        </AlertDialogCancel>
+                                        <AlertDialogCancel
+                                        className="bg-teal-600 cursor-pointer text-white"
+                                        type="submit"
+                                        onClick={() =>
+                                            handleSubmit(handleQuestions)()
+                                        }
+                                        >
+                                        Continue
+                                        </AlertDialogCancel>
+                                    </AlertDialogFooter>
+                                    </AlertDialogContent>
+                                </AlertDialog>
+                                )}
+                            </div>
+                            </form>
+                        </CardContent>
+                        </Card>
+                    </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+                </Carousel>
+            </div>
         </div>
+    </div>
   )
 }
 
