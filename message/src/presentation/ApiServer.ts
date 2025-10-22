ba112b3af94cbadf9e7542bdd3c9058b90826d5e
ansifpk <pkansif39@gmail.com>
2025-10-22 14:59:39 +0530

    checking envs

--- message/src/presentation/ApiServer.ts
+++ message/src/presentation/ApiServer.ts
@@ -28,9 +28,10 @@ export class ApiServer {
          
          const httpServer = createServer(app);
          const allowedOrgins =  JSON.parse(process.env.ORGINS!)
-         console.log("allowedOrgins",allowedOrgins);
-         console.log("process.env.ORGINS",process.env.ORGINS);
+         console.log("allowedOrgins",process.env.KAFKA_BROKER);
+         console.log("allowedOrgins",process.env);
          
+
          app.set('trust proxy',true);
          app.use(cookieParser())
          app.use(cors({credentials:true,
