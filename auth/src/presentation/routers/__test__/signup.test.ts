2ce254416ef0a4e29186c0508a671d5cc8252dd4
ansifpk <pkansif39@gmail.com>
2025-12-30 21:27:27 +0530

    added fake nodemailer

--- auth/src/presentation/routers/__test__/signup.test.ts
+++ auth/src/presentation/routers/__test__/signup.test.ts
@@ -1,6 +1,12 @@
 import request from 'supertest';
 import { httpServer } from '../../app';
 
+jest.mock('nodemailer', () => ({
+  createTransport: () => ({
+    sendMail: jest.fn()
+  })
+}));
+
 it("reqtuen a success signup",async()=>{
     return request(httpServer)
     .post('/auth/user/register')
