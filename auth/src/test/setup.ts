84d2bed8e006d97e3119158b60beecd8e0d3c8c1
ansifpk <pkansif39@gmail.com>
2025-12-30 21:32:08 +0530

    fixed

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -17,12 +17,13 @@ beforeAll(async()=>{
     await mongoose.connect(mongoUri);
 });
 beforeEach(async()=>{
+   
+})
+afterAll(async()=>{
    const collections = await mongoose.connection.db!.collections();
    for (let collection of collections) {
       await collection.deleteMany()
    }
-})
-afterAll(async()=>{
-   await mongoose.connection.close()
    await mongo.stop()
+   await mongoose.connection.close()
 });
