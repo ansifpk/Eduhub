266a76a7b14d4d4b0991af99f2a07b7709158e04
ansifpk <pkansif39@gmail.com>
2025-12-31 16:52:29 +0530

    added moke folder

--- auth/src/presentation/routers/__test__/signup.test.ts
+++ auth/src/presentation/routers/__test__/signup.test.ts
@@ -1,20 +1,8 @@
 import request from 'supertest';
 import { app } from '../../../app';
 
-// Move the mock outside and clean it up properly
-// const sendMailMock =  jest.fn().mockResolvedValue({ messageId: 'test-id' });
-// const closeMock = jest.fn();
-// jest.mock('nodemailer', () => ({
-//   createTransport: jest.fn(() => ({
-//     sendMail: sendMailMock,
-//     close: closeMock,
-//   }))
-// }));
+jest.mock('nodemailer');
 
-// describe('User Signup', () => {
-//   afterEach(() => {
-//     jest.clearAllMocks(); // Clear mocks after each test
-//   });
 
   it("returns a success signup", async () => {
     return request(app)
