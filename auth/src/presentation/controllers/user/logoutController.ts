7d2dea1777cf1c97503dcc1600f42664fea64405
ansifpk <pkansif39@gmail.com>
2026-03-24 20:48:37 +0530

    fixed token issue

--- auth/src/presentation/controllers/user/logoutController.ts
+++ auth/src/presentation/controllers/user/logoutController.ts
@@ -5,21 +5,24 @@ import { StatusCodes } from "@eduhublearning/common";
 export class LogOutController implements IController {
 
     public async handle(req: Request, res: Response): Promise<void> {
-       try {
-            res.cookie('accessToken','',{
-                httpOnly:true,
-                expires:new Date(0)
+        try {
+            res.clearCookie('accessToken', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                sameSite: process.env.NODE_ENV == 'development' ? 'strict' : 'none',
+                path: '/',
             });
-            res.cookie('refreshToken','',{
-                httpOnly:true,
-                expires:new Date(0)
+
+            res.clearCookie('refreshToken', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                sameSite: process.env.NODE_ENV == 'development' ? 'strict' : 'none',
+                path: '/',
             });
-            res.clearCookie("accessToken")
-                res.clearCookie("refreshToken")
             res.status(StatusCodes.OK).send({ succuss: true, message: "logout success" });
-       } catch (error) {
-         console.error(error);
-       }
+        } catch (error) {
+            console.error(error);
+        }
     }
-    
+
 }
